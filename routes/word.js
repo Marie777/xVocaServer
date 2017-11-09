ed8453e608fc3a1a02010146f668f6ce63adc7f4
Reut Levy <settrue0@gmail.com>
2017-11-09 18:45:14 +0200

    routes

--- routes/word.js
+++ routes/word.js
@@ -14,24 +14,38 @@ router.get('/:word', async (req, res) => {
     return res.json(wordDetails);
   }
 
+
 });
 
 router.post('/mockWord', async (req, res) => {
 
   let sentence1 = {
       sentence: "sentence1",
-      user: "5a009c07c22ed764907cae95", //reference?
+      user: "5a009c07c22ed764907cae95",
       countLike: 3,
       location: {lat: 50, lng: 30}
   }
+  let sentence2 = {
+      sentence: "sentence2",
+      user: "5a009c07c22ed764907cae95",
+      countLike: 3,
+      location: {lat: 30, lng: 30}
+  }
+  let sentence3 = {
+      sentence: "sentence3",
+      user: "5a009c07c22ed764907cae95",
+      countLike: 3,
+      location: {lat: 0, lng: 30}
+  }
   let mockWord = {
-      word: "blal_1",
+      word: "word1",
       translate: "translation...",
       images: ["img1", "img2"],
-      sentences: [sentence1, sentence1]
+      sentences: [sentence1, sentence2, sentence3]
   }
 
   const wordDetails = await WordDetails.create(mockWord);
+
 
   res.json(wordDetails);
 });
