571eb6c8cf61e8345d19ce281907edcc2504470d
Reut Levy <settrue0@gmail.com>
2018-04-02 13:44:25 +0300

    Watson async lib

--- routes/analyzetxt.js
+++ routes/analyzetxt.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import _ from 'lodash';
-import pdfUtil from 'pdf-to-text';
-import watsonNLU from 'watson-developer-cloud/natural-language-understanding/v1.js';
+//import pdfUtil from 'pdf-to-text';
+import watsonNLU from 'watson-developer-cloud-async/natural-language-understanding/v1.js';
 import thesaurus from 'thesaurus-com';
 import wordnet from 'wordnet';
 import wordnetSQlite from 'wordnet-sqlite';
@@ -9,10 +9,6 @@ import CoreNLP, { Properties, Pipeline, ConnectorServer } from 'corenlp';
 
 const router = Router();
 
-//CoreNLP connection:
-const connector = new ConnectorServer({ dsn: 'http://localhost:9000' });
-const props = new Properties({ annotators: 'tokenize,ssplit,pos,lemma,ner,parse'});
-const pipeline = new Pipeline(props, 'English', connector);
 
 //Watson connection
 const natural_language_understanding = new watsonNLU({
@@ -20,22 +16,31 @@ const natural_language_understanding = new watsonNLU({
   'password': '50wlIuqqetil',
   'version_date': '2017-02-27'
 });
-const parameters = {
-  //'url': 'http://research.ibm.com/tjbot/?lnk=ushpv18f2&lnk2=learn',
-  'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
-  'features': {
-    'categories': {}
-  }
+
+//Watson request
+const watsonCategory = async (text) => {
+  const parameters = {
+    //'url': 'http://research.ibm.com/tjbot/?lnk=ushpv18f2&lnk2=learn',
+    text,
+    'features': {
+      'categories': {}
+    }
+  };
+
+  try{
+      return await natural_language_understanding.analyze(parameters);
+    }catch(err){
+      return err;
+    }
 };
 
-//TODO: Input text (PDF)
-const rowtxt = {
-  text : "The problem problem  related to evaluation of subjective subjective answers is that each student has his/her own way of answering and it is difficult to determine the degree of correctness [1]. The assessment of the correctness of an answer involves the evaluation of grammar and knowledge woven using the conceived interpretation and creativity of a human mind. Human Evaluation, though slow and carrying drawbacks of human fatigue and bias is the only accepted method 12ba3 for evaluation of text based answers, the intelligence of one human can be fathomed by another. However, with the development of communication and internet technologies, the reach and nature of education has changed with its spread across geographical, social and political boundaries with an exponential growth of intake volume. This has made the drawbacks of human evolution come out more glaring than ever before and interfere with the importance of",
-  text22:"based answers, the intelligence of one",
-  text2: "The purpose of pharmacy compounding has traditionally been to allow a licensed pharmacist to customize a medication for an individual patient whose needs cannot be met by an FDA-approved drug. For example, a patient who is allergic to a certain dye in an FDA-approved drug may need a drug compounded without that ingredient. Similarly, a liquid-compounded drug may best meet the needs of a child or elderly patient who cannot swallow an FDA-approved tablet or capsule. Such prescription-based, individualized compounding by pharmacies continues to fill a niche that mass-produced pharmaceuticals cannot fill. "
-};
 
+//CoreNLP connection:
+const connector = new ConnectorServer({ dsn: 'http://localhost:9000' });
+const props = new Properties({ annotators: 'tokenize,ssplit,pos,lemma,ner,parse'});
+const pipeline = new Pipeline(props, 'English', connector);
 
+//CoreNLP request
 const corenlpTags = async (text) => {
   const sent = new CoreNLP.simple.Sentence(text);
   const resultCore = await pipeline.annotate(sent);
@@ -49,6 +54,7 @@ const corenlpTags = async (text) => {
       return err;
     }
 };
+
 
 const wordFrequency = (text) => {
   //const splittxt = _.split(rowtxt.text, ' ');
@@ -69,6 +75,19 @@ const wordFrequency = (text) => {
 };
 
 
+
+
+
+//TODO: Input text (PDF)
+const rowtxt = {
+  text : "The problem problem  related to evaluation of subjective subjective answers is that each student has his/her own way of answering and it is difficult to determine the degree of correctness [1]. The assessment of the correctness of an answer involves the evaluation of grammar and knowledge woven using the conceived interpretation and creativity of a human mind. Human Evaluation, though slow and carrying drawbacks of human fatigue and bias is the only accepted method 12ba3 for evaluation of text based answers, the intelligence of one human can be fathomed by another. However, with the development of communication and internet technologies, the reach and nature of education has changed with its spread across geographical, social and political boundaries with an exponential growth of intake volume. This has made the drawbacks of human evolution come out more glaring than ever before and interfere with the importance of",
+  text22:"based answers, the intelligence of one",
+  text2: "The purpose of pharmacy compounding has traditionally been to allow a licensed pharmacist to customize a medication for an individual patient whose needs cannot be met by an FDA-approved drug. For example, a patient who is allergic to a certain dye in an FDA-approved drug may need a drug compounded without that ingredient. Similarly, a liquid-compounded drug may best meet the needs of a child or elderly patient who cannot swallow an FDA-approved tablet or capsule. Such prescription-based, individualized compounding by pharmacies continues to fill a niche that mass-produced pharmaceuticals cannot fill. "
+
+};
+
+
+
 //-----------------------
 router.get('/', async (req, res) => {
 
@@ -78,11 +97,18 @@ router.get('/', async (req, res) => {
   //       throw(err);
   //     }
 
+      //Watson:
+      //const categoryAnalysis = await watsonCategory(rowtxt.text); //TODO: uncomment
+      //res.send(JSON.stringify(await watsonCategory(rowtxt.text)));
+
       //Frequent Words:
       let listWords = wordFrequency(rowtxt.text); //textFromPDF);
 
       //CoreNLP:
       const wordPosTags = await corenlpTags(rowtxt.text);
+      res.send(wordPosTags);
+
+
       _.forEach(listWords, (w) => {
         w.posCoreNLP = [];
         _.forEach(wordPosTags, (t) => {
@@ -92,7 +118,9 @@ router.get('/', async (req, res) => {
         });
       });
 
-        //WordNet - definition, type:
+      // res.send(wordPosTags);
+
+       // WordNet - definition, type:
         const wordString = listWords.map(w => `'${w.word.toString()}'`).join(', ');
         const query = `SELECT * FROM words WHERE word IN (${wordString});`;
         wordnetSQlite.all(query, (err, rows) => {
@@ -105,14 +133,6 @@ router.get('/', async (req, res) => {
                 w.definition = dict[w.word] && dict[w.word].definition;
                 w.type = dict[w.word] && dict[w.word].type;
             });
-
-            //Watson:
-            // natural_language_understanding.analyze(parameters, (err, response) => {
-            //   if (err)
-            //     return err;
-            //   else{
-            //       _.forEach(listWords, (w) => { w.categoryWatson = response.categories});
-
                   //Weight:
                   _.forEach(listWords, (w) => {
                     const typeValue = wordNetType[w.type] ? wordNetType[w.type] : 0.1;
@@ -120,15 +140,17 @@ router.get('/', async (req, res) => {
                     w.totalWeight = posCoreNLPValue * typeValue;
                   });
 
+                  //add each word? paragraph? Watson category:
+                  //_.forEach(listWords, (w) => { w.categoryWatson = categoryAnalysis.categories}); //TODO: uncomment
+
                   //orderBy
                   let sortedWords = _.orderBy(listWords, ['totalWeight', 'count'], ['desc', 'desc']);
                   // console.log(listWords);
+                  //--- result ---
                   res.send(sortedWords);
-            //   }
-            // });
+
           }
         });
-
 
 
       //_.forEach(listWords, (w) => {w.thesaurus = thesaurus.search(w)});
@@ -138,17 +160,6 @@ router.get('/', async (req, res) => {
 });
 
 
-
-
-
-router.get('/watson', async (req, res) => {
-  natural_language_understanding.analyze(parameters, (err, response) => {
-    if (err)
-      res.send(err);
-    else
-      res.send(JSON.stringify(response, null, 2));
-  });
-});
 
 
 router.get('/wordNet', async (req, res) => {
