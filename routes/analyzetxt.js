2c68673742830fa1084750f2b60269cfa65851a9
Reut Levy <settrue0@gmail.com>
2018-04-14 14:02:35 +0300

    coreNLP promise reject fix

--- routes/analyzetxt.js
+++ routes/analyzetxt.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import _ from 'lodash';
-import pdfUtil from 'pdf-to-text';
 import fs from 'fs';
 import PDFParser from 'pdf2json';
 import watsonNLU from 'watson-developer-cloud-async/natural-language-understanding/v1.js';
@@ -54,7 +53,23 @@ const corenlpTags = async (text) => {
     }
 };
 
+//CoreNLP analysis per sentence
+const coreNLPAnalysis = async (splitSentences) => {
+  const posTaging = [];
+  for (let s of splitSentences) {
+    try {
+      const pos = await corenlpTags(s);
+      posTaging.push(pos);
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
+  return [].concat.apply([], posTaging);
+}
 
+
+//Create set of words from text and calculates their frequency
 const wordFrequency = (text) => {
   //const splittxt = _.split(rowtxt.text, ' ');
   //const uniqWords = _.uniq(words);
@@ -62,7 +77,7 @@ const wordFrequency = (text) => {
   const wordsNoInt = _.filter(words, (e) =>  { return !_.isInteger(_.parseInt(e)) && e.length > 1});
   const wordsCount = _.countBy(wordsNoInt);
 
-  const wordFrequency = _.map(wordsCount, (value,key) => { return {word:key, count:value, definition:"", type:"", totalWeight:null, posCoreNLP:[]} });
+  const wordFrequency = _.map(wordsCount, (value,key) => { return {word:key, count:value, definition:"", type:"", totalWeight:null, posCoreNLP:[], namedEntities:[]} });
   _.orderBy(wordFrequency, ['count'], ['desc']);
 
   return wordFrequency.reduce((accu, currItem) => {
@@ -84,27 +99,8 @@ const readPDF = async (pdfFilePath) => {
 };
 
 
-const readPDF1 = async (pdfFilePath) => {
-  const text = await readPDF(pdfFilePath);
-  if(text){
-    return text;
-  }else{
-    return null;
-  }
-};
 
 
-
-
-
-
-//TODO: Input text (PDF)
-const rowtxt = {
-  text : "The problem problem  related to evaluation of subjective subjective answers is that each student has his/her own way of answering and it is difficult to determine the degree of correctness [1]. The assessment of the correctness of an answer involves the evaluation of grammar and knowledge woven using the conceived interpretation and creativity of a human mind. Human Evaluation, though slow and carrying drawbacks of human fatigue and bias is the only accepted method 12ba3 for evaluation of text based answers, the intelligence of one human can be fathomed by another. However, with the development of communication and internet technologies, the reach and nature of education has changed with its spread across geographical, social and political boundaries with an exponential growth of intake volume. This has made the drawbacks of human evolution come out more glaring than ever before and interfere with the importance of",
-  text22:"based answers, the intelligence of one",
-  text2: "The purpose of pharmacy compounding has traditionally been to allow a licensed pharmacist to customize a medication for an individual patient whose needs cannot be met by an FDA-approved drug. For example, a patient who is allergic to a certain dye in an FDA-approved drug may need a drug compounded without that ingredient. Similarly, a liquid-compounded drug may best meet the needs of a child or elderly patient who cannot swallow an FDA-approved tablet or capsule. Such prescription-based, individualized compounding by pharmacies continues to fill a niche that mass-produced pharmaceuticals cannot fill. "
-
-};
 
 
 
@@ -113,63 +109,62 @@ const rowtxt = {
 router.get('/', async (req, res) => {
 
     const pdfFilePath = "./examlePDF.pdf";
-    const inputTxt = rowtxt.text; // await readPDF(pdfFilePath);
+    const inputTxt = await readPDF(pdfFilePath); //rowtxt.text; //
     if(inputTxt){
 
 
+      //Watson:
+      //const categoryAnalysis = await watsonCategory(rowtxt.text); //TODO: uncomment
+      //res.send(JSON.stringify(await watsonCategory(rowtxt.text)));
+
+      //Frequent Words:
+      let listWords = wordFrequency(inputTxt);
+
+      const splitSentences = _.split(inputTxt, '.');
+
+      const posTaging = await coreNLPAnalysis(splitSentences);
+
+      posTaging.forEach(item => {
+        if(listWords[item.word.toLowerCase()])
+          listWords[item.word.toLowerCase()].posCoreNLP.push(item.pos)
+     });
+
+      //res.send(listWords);
 
 
-    //Watson:
-    //const categoryAnalysis = await watsonCategory(rowtxt.text); //TODO: uncomment
-    //res.send(JSON.stringify(await watsonCategory(rowtxt.text)));
+       // WordNet - definition, type:
+        const wordString = Object.keys(listWords).map(key => `'${key.toString()}'`).join(', ');
+        const query = `SELECT * FROM words WHERE word IN (${wordString});`;
+        wordnetSQlite.all(query, (err, rows) => {
+          if(err){
+            console.log(err);
+          }else{
+            rows.forEach(item => {
+              if(listWords[item.word.toLowerCase()]){
+              listWords[item.word.toLowerCase()].definition = item.definition;
+              listWords[item.word.toLowerCase()].type = item.type;
+              }
+            });
+          }
 
-    //Frequent Words:
-    let listWords = wordFrequency(inputTxt);
-
-    //CoreNLP:
-    const splitSentences = _.split(inputTxt, '.');
-    const promises = splitSentences.map(s => corenlpTags(s));
-    const posTaging = await Promise.all(promises);
-
-    var merged = [].concat.apply([], posTaging);
-
-    merged.forEach(item => {
-      if(listWords[item.word.toLowerCase()])
-        listWords[item.word.toLowerCase()].posCoreNLP.push(item.pos)
-   });
-
-
-     // WordNet - definition, type:
-      const wordString = Object.keys(listWords).map(key => `'${key.toString()}'`).join(', ');
-      const query = `SELECT * FROM words WHERE word IN (${wordString});`;
-      wordnetSQlite.all(query, (err, rows) => {
-        if(err){
-          console.log(err);
-        }else{
-          rows.forEach(item => {
-            if(listWords[item.word.toLowerCase()]){
-            listWords[item.word.toLowerCase()].definition = item.definition;
-            listWords[item.word.toLowerCase()].type = item.type;
-            }
+          //Calc weight for word:
+          Object.keys(listWords).forEach((key) => {
+            const word = listWords[key];
+            const typeValue = wordNetType[word.type] ? wordNetType[word.type] : 1;
+            const posCoreNLPValue = pennPOS[word.posCoreNLP[0]] ? pennPOS[word.posCoreNLP[0]] : 0.1;
+            const namedEntitiesValue = 0;
+            listWords[key].totalWeight = posCoreNLPValue * typeValue; // * entities
           });
-        }
 
-        Object.keys(listWords).forEach((key) => {
-          const word = listWords[key];
-          const typeValue = wordNetType[word.type] ? wordNetType[word.type] : 1;
-          const posCoreNLPValue = pennPOS[word.posCoreNLP[0]] ? pennPOS[word.posCoreNLP[0]] : 0.1;
-          listWords[key].totalWeight = posCoreNLPValue * typeValue; //divide not multiple
+          //orderBy list words
+          let sortedWords = _.orderBy(listWords, ['totalWeight', 'count'], ['desc', 'desc']);
+          res.send(sortedWords);
+
         });
-
-        //orderBy
-        let sortedWords = _.orderBy(listWords, ['totalWeight', 'count'], ['desc', 'desc']);
-        res.send(sortedWords);
-
-      });
 
     //_.forEach(listWords, (w) => {w.thesaurus = thesaurus.search(w)});
     //console.log(listWords);
-}
+  }
 
 });
 
@@ -208,6 +203,12 @@ const wordNetType = {
   adv : 0.6
 };
 
+const namedEntities = {
+  person : 0,
+  date : 0,
+  organization : 0,
+  number : 0
+}
 const pennPOS = {
   CC	: 0,
   CD	: 0.1,
@@ -248,6 +249,32 @@ const pennPOS = {
 };
 
 export default router;
+
+
+// //TODO: Input text (PDF)
+// const rowtxt = {
+//   text : "The problem problem  related to evaluation of subjective subjective answers is that each student has his/her own way of answering and it is difficult to determine the degree of correctness [1]. The assessment of the correctness of an answer involves the evaluation of grammar and knowledge woven using the conceived interpretation and creativity of a human mind. Human Evaluation, though slow and carrying drawbacks of human fatigue and bias is the only accepted method 12ba3 for evaluation of text based answers, the intelligence of one human can be fathomed by another. However, with the development of communication and internet technologies, the reach and nature of education has changed with its spread across geographical, social and political boundaries with an exponential growth of intake volume. This has made the drawbacks of human evolution come out more glaring than ever before and interfere with the importance of",
+//   text22:"based answers, the intelligence of one",
+//   text2: "The purpose of pharmacy compounding has traditionally been to allow a licensed pharmacist to customize a medication for an individual patient whose needs cannot be met by an FDA-approved drug. For example, a patient who is allergic to a certain dye in an FDA-approved drug may need a drug compounded without that ingredient. Similarly, a liquid-compounded drug may best meet the needs of a child or elderly patient who cannot swallow an FDA-approved tablet or capsule. Such prescription-based, individualized compounding by pharmacies continues to fill a niche that mass-produced pharmaceuticals cannot fill. "
+//
+// };
+
+
+// //CoreNLP:
+// const splitSentences = _.split(inputTxt, '.');
+// const promises = splitSentences.map(s => corenlpTags(s));
+// const posTaging = await Promise.all(promises);
+//
+
+
+// const readPDF1 = async (pdfFilePath) => {
+//   const text = await readPDF(pdfFilePath);
+//   if(text){
+//     return text;
+//   }else{
+//     return null;
+//   }
+// };
 
 
 // router.get('/pdfjsn', async (req, res) => {
